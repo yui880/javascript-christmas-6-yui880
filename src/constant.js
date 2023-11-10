39ecf3b101c87cb18b0436b7c4162958eaa0a4ca
yui880 <kimyuna521@gmail.com>
2023-11-10 12:19:06 +0900

    feat(constant): 사용자에게 출력될 메세지들에 대한 상수 선언

--- src/constant.js
+++ src/constant.js
@@ -65,3 +65,26 @@ export const MENU_PRIZE = {
   dessert: MENU.dessert.map((food) => food.price),
   drink: MENU.drink.map((food) => food.price),
 };
+
+export const MESSAGE = {
+  welcome: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
+  enterDate: '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+  enterMenu:
+    '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+  preview: (day) => `12월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
+};
+
+export const EVENT_TITLE = {
+  menu: '\n<주문 메뉴>\n',
+  beforeDiscount: '\n<할인 전 총주문 금액>\n',
+  gift: '\n<증정 메뉴>\n',
+  benefitList: '\n<혜택 내역>\n',
+  totalBenefit: '\n<총혜택 금액>\n',
+  afterDiscount: '\n<할인 후 예상 결제 금액>\n',
+  badge: '\n<12월 이벤트 배지>\n',
+};
+
+export const COUNT_UNIT = {
+  item: '개',
+  money: '원',
+};
