54cf8b0157de0377b1cab7419b4d6b5925692c6d
yui880 <kimyuna521@gmail.com>
2023-11-10 21:47:49 +0900

    feat(Validator): 날짜가 1과 31 사이의 숫자가 아니면 예외 발생 구현

---
+++ src/Validator.js
@@ -0,0 +1,12 @@
+import { ERROR, PERIOD } from './constant.js';
+import ValidationError from './ValidationError.js';
+
+const Validator = {
+  checkIsInValidDateRange(date) {
+    if (Number(date) < PERIOD.promotion.start || Number(date) > PERIOD.promotion.end) {
+      throw new ValidationError(ERROR.invalidDate);
+    }
+  },
+};
+
+export default Validator;
