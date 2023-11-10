cf3a62f5f0f11e6a104ecd15999248ccf5228a9d
yui880 <kimyuna521@gmail.com>
2023-11-10 22:53:48 +0900

    feat(Validator): 메뉴에 없는 음식이 들어오면 예외 발생

--- src/Validator.js
+++ src/Validator.js
@@ -1,14 +1,21 @@
-import { ERROR, PERIOD } from './constant.js';
+import { ALL_MENU_NAME, ERROR, MENU_NAME, PERIOD, RANGE } from './constant.js';
 import ValidationError from './ValidationError.js';
 
 const Validator = {
-  checkIsInValidDateRange(date) {
+  validateDate(date) {
     if (Number(date) < PERIOD.promotion.start || Number(date) > PERIOD.promotion.end) {
       throw new ValidationError(ERROR.invalidDate);
     }
     if (!Number.isInteger(Number(date))) {
       throw new ValidationError(ERROR.invalidDate);
     }
+  },
+  checkIsNameInMenu(names) {
+    names.forEach((name) => {
+      if (!ALL_MENU_NAME.includes(name)) {
+        throw new ValidationError(ERROR.invalidDate);
+      }
+    });
   },
 };
 
