93262b6a357de692d39979af81b5a416247ed274
yui880 <kimyuna521@gmail.com>
2023-11-10 12:22:22 +0900

    feat(InputView): 사용자에게 날짜와 메뉴를 입력받는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,18 @@
-export default InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+import { MESSAGE } from './constant.js';
+
+const InputView = {
+  async readDate() {
+    const date = await Console.readLineAsync(MESSAGE.enterDate);
+
+    return date;
+  },
+
+  async readMenu() {
+    const menu = await Console.readLineAsync(MESSAGE.enterMenu);
+
+    return menu;
+  },
+};
+
+export default InputView;
