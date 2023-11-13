2c3c67e6dda6d350547504dd49a93e7e36e9d899
yui880 <kimyuna521@gmail.com>
2023-11-13 23:33:01 +0900

    test(Discount): 이벤트 적용한 값을 여러 방식으로 구하는 기능 테스트 작성

--- __tests__/DiscountTest.js
+++ __tests__/DiscountTest.js
@@ -1,5 +1,6 @@
 import Discount from '../src/domain/Discount.js';
-import { DISCOUNT_EVENT, PERIOD } from '../src/constant.js';
+import { DISCOUNT_EVENT, DISCOUNT_STANDARD, PERIOD } from '../src/constant.js';
+import Product from '../src/domain/Product.js';
 
 const getCustomProductMock = ({
   isPriceLessThan = false,
@@ -29,6 +30,96 @@ describe('Discount 클래스 테스트', () => {
     // then
     const expectedResult = new Array(Object.keys(DISCOUNT_EVENT).length).fill(0);
     expect(amountList).toEqual(expectedResult);
+  });
+
+  test.each([
+    {
+      dessertCount: 1,
+      mainCount: 2,
+      day: 8, // 금요일
+      output: 5746, // 주말 4046 + 크리스마스 1700
+    },
+    {
+      dessertCount: 1,
+      mainCount: 0,
+      day: 10, // 일요일, 별
+      output: 4923, // 평일 2023 + 크리스마스 1900 + 특별 1000
+    },
+  ])('총 할인 금액을 구하는 기능 테스트', ({ dessertCount, mainCount, day, output }) => {
+    // given
+    const productMock = getCustomProductMock({
+      isPriceLessThan: false,
+      dessertOrderCount: dessertCount,
+      mainOrderCount: mainCount,
+    });
+    discount.apply(productMock, day);
+
+    // when
+    const result = discount.getAmount();
+
+    // then
+    expect(result).toBe(output);
+  });
+
+  test.each([
+    {
+      dessertCount: 1,
+      mainCount: 2,
+      day: 8, // 금요일
+      output: false,
+    },
+    {
+      dessertCount: 0,
+      mainCount: 0,
+      day: 10,
+      output: false,
+    },
+  ])(
+    '총 할인 금액이 0인지 아닌지 구하는 기능 테스트',
+    ({ dessertCount, mainCount, day, output }) => {
+      // given
+      const productMock = getCustomProductMock({
+        isPriceLessThan: false,
+        dessertOrderCount: dessertCount,
+        mainOrderCount: mainCount,
+      });
+      discount.apply(productMock, day);
+
+      // when
+      const result = discount.isEmpty();
+
+      // then
+      expect(result).toBe(output);
+    },
+  );
+
+  test.each([
+    {
+      day: 1,
+      dayOfWeek: 5, // 금요일
+    },
+    {
+      day: 31,
+      dayOfWeek: 0, // 일요일
+    },
+  ])('날짜를 입력하면 무슨 요일인지 알려주는 기능 테스트(12월 한정)', ({ day, dayOfWeek }) => {
+    // when
+    const result = discount.getDayOfWeek(day);
+
+    // then
+    expect(result).toBe(dayOfWeek);
+  });
+});
+
+describe('Discount 할인 이벤트 테스트', () => {
+  let discount;
+
+  beforeEach(() => {
+    discount = new Discount();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   test.each([
@@ -79,6 +170,8 @@ describe('Discount 클래스 테스트', () => {
 
       // when
       const result = discount.getAmountByEvent()[1];
+
+      // then
       expect(result).toBe(output);
     },
   );
@@ -98,6 +191,8 @@ describe('Discount 클래스 테스트', () => {
 
       // when
       const result = discount.getAmountByEvent()[1];
+
+      // then
       expect(result).toBe(output);
     },
   );
@@ -117,6 +212,8 @@ describe('Discount 클래스 테스트', () => {
 
       // when
       const result = discount.getAmountByEvent()[2];
+
+      // then
       expect(result).toBe(output);
     },
   );
@@ -136,6 +233,8 @@ describe('Discount 클래스 테스트', () => {
 
       // when
       const result = discount.getAmountByEvent()[2];
+
+      // then
       expect(result).toBe(output);
     },
   );
@@ -152,13 +251,15 @@ describe('Discount 클래스 테스트', () => {
 
     // when
     const result = discount.getAmountByEvent()[3];
+
+    // then
     expect(result).toBe(output);
   });
 
   test.each([
     { day: 1, output: 0 },
     { day: 30, output: 0 },
-  ])('별이 있는 날일 때 특별할인이 적용되는지 테스트', ({ day, output }) => {
+  ])('별이 없는 날일 때 특별할인이 적용되지 않는 테스트', ({ day, output }) => {
     // given
     const productMock = getCustomProductMock({
       isPriceLessThan: false,
@@ -167,6 +268,66 @@ describe('Discount 클래스 테스트', () => {
 
     // when
     const result = discount.getAmountByEvent()[3];
+
+    // then
     expect(result).toBe(output);
   });
+
+  test.each([
+    {
+      dessertCount: 1,
+      mainCount: 2,
+      day: 8, // 금요일
+      output: [1700, 0, 4046, 0], // 주말 4046 + 크리스마스 1700
+    },
+    {
+      dessertCount: 1,
+      mainCount: 0,
+      day: 10, // 일요일, 별
+      output: [1900, 2023, 0, 1000], // 평일 2023 + 크리스마스 1900 + 특별 1000
+    },
+  ])(
+    '여러가지 할인 이벤트가 제대로 적용되는지 테스트',
+    ({ dessertCount, mainCount, day, output }) => {
+      // given
+      const productMock = getCustomProductMock({
+        isPriceLessThan: false,
+        dessertOrderCount: dessertCount,
+        mainOrderCount: mainCount,
+      });
+      discount.apply(productMock, day);
+
+      // when
+      const result = discount.getAmountByEvent();
+
+      // then
+      expect(result).toEqual(output);
+    },
+  );
+
+  test.each([
+    {
+      product: new Product([['아이스크림', 1]]), //
+      output: [0, 0, 0, 0],
+    },
+    {
+      product: new Product([
+        ['양송이수프', 1],
+        ['제로콜라', 1],
+      ]),
+      output: [0, 0, 0, 0],
+    },
+  ])(
+    `총 가격이 ${DISCOUNT_STANDARD.minimum}을 넘지 않으면 이벤트가 적용되지 않는지 테스트`,
+    ({ product, output }) => {
+      // given
+      discount.apply(product, 1);
+
+      // when
+      const result = discount.getAmountByEvent();
+
+      // then
+      expect(result).toEqual(output);
+    },
+  );
 });
